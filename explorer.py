0ecbd76274d4641dd2f12692e945e5f87cf7a7b2
Linux_Hat <adrien@d4x.fr>
2024-05-16 18:32:04 +0200

    fixes

--- explorer.py
+++ explorer.py
@@ -46,59 +46,62 @@ db.commit()
 
 def ping_server(port):
     while True :
-        if event.is_set() :
-            break
-        addr = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
         try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.settimeout(timeout/1000)
-                s.connect((addr, port))
-        except :
-            continue
-        
-        try:
-            server = JavaServer.lookup(f"{addr}:{port}")
-            status = server.status()
+            if event.is_set() :
+                break
+            addr = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.settimeout(timeout/1000)
+                    s.connect((addr, port))
+            except :
+                continue
+            
+            try:
+                server = JavaServer.lookup(f"{addr}:{port}")
+                status = server.status()
+            except:
+                continue
+
+            if status:
+                players = f"{status.players.online}/{status.players.max}"
+                
+                ip = f"{addr}:{port}"
+                
+                version=str(status.version.name)
+                if len(version) > 27:
+                    version = f"{version[:27-3]}..."
+                
+                latency = str(int(status.latency))+"ms"
+
+                description = status.description
+                description = description.replace("§1",colorama.Fore.BLUE)
+                description = description.replace("§2",colorama.Fore.GREEN)
+                description = description.replace("§3",colorama.Fore.CYAN)
+                description = description.replace("§4",colorama.Fore.RED)
+                description = description.replace("§5",colorama.Fore.MAGENTA)
+                description = description.replace("§6",colorama.Fore.YELLOW)
+                description = description.replace("§7",colorama.Fore.LIGHTBLACK_EX)
+                description = description.replace("§8",colorama.Fore.LIGHTBLACK_EX)
+                description = description.replace("§9",colorama.Fore.LIGHTBLUE_EX)
+                description = description.replace("§a",colorama.Fore.LIGHTGREEN_EX)
+                description = description.replace("§b",colorama.Fore.LIGHTCYAN_EX)
+                description = description.replace("§c",colorama.Fore.LIGHTRED_EX)
+                description = description.replace("§d",colorama.Fore.LIGHTMAGENTA_EX)
+                description = description.replace("§e",colorama.Fore.LIGHTYELLOW_EX)
+                description = description.replace("§f",colorama.Fore.WHITE)
+                description = description.replace("\n"," - ")
+                description = description.replace("§k","")
+                description = description.replace("§l",colorama.Style.BRIGHT)
+                description = description.replace("§m","")
+                description = description.replace("§n","\e[4m")
+                description = description.replace("§o","")
+                description = description.replace("§r",colorama.Style.RESET_ALL)
+
+                results.append([addr, port, f"{colorama.Fore.LIGHTRED_EX}{players:10}{colorama.Fore.LIGHTBLUE_EX}{ip:22}{colorama.Fore.LIGHTYELLOW_EX}{version:30}{colorama.Fore.LIGHTGREEN_EX}{latency:6}{colorama.Style.RESET_ALL}{description}{colorama.Style.RESET_ALL}"])
         except:
-            continue
+            pass
 
-        if status:
-            players = f"{status.players.online}/{status.players.max}"
-            
-            ip = f"{addr}:{port}"
-            
-            version=str(status.version.name)
-            if len(version) > 27:
-                version = f"{version[:27-3]}..."
-            
-            latency = str(int(status.latency))+"ms"
-
-            description = status.description
-            description = description.replace("§1",colorama.Fore.BLUE)
-            description = description.replace("§2",colorama.Fore.GREEN)
-            description = description.replace("§3",colorama.Fore.CYAN)
-            description = description.replace("§4",colorama.Fore.RED)
-            description = description.replace("§5",colorama.Fore.MAGENTA)
-            description = description.replace("§6",colorama.Fore.YELLOW)
-            description = description.replace("§7",colorama.Fore.LIGHTBLACK_EX)
-            description = description.replace("§8",colorama.Fore.LIGHTBLACK_EX)
-            description = description.replace("§9",colorama.Fore.LIGHTBLUE_EX)
-            description = description.replace("§a",colorama.Fore.LIGHTGREEN_EX)
-            description = description.replace("§b",colorama.Fore.LIGHTCYAN_EX)
-            description = description.replace("§c",colorama.Fore.LIGHTRED_EX)
-            description = description.replace("§d",colorama.Fore.LIGHTMAGENTA_EX)
-            description = description.replace("§e",colorama.Fore.LIGHTYELLOW_EX)
-            description = description.replace("§f",colorama.Fore.WHITE)
-            description = description.replace("\n"," - ")
-            description = description.replace("§k","")
-            description = description.replace("§l",colorama.Style.BRIGHT)
-            description = description.replace("§m","")
-            description = description.replace("§n","\e[4m")
-            description = description.replace("§o","")
-            description = description.replace("§r",colorama.Style.RESET_ALL)
-
-            results.append([addr, port, f"{colorama.Fore.LIGHTRED_EX}{players:10}{colorama.Fore.LIGHTBLUE_EX}{ip:22}{colorama.Fore.LIGHTYELLOW_EX}{version:30}{colorama.Fore.LIGHTGREEN_EX}{latency:6}{colorama.Style.RESET_ALL}{description}{colorama.Style.RESET_ALL}"])
-    
 def Main():
     try :
         compter = [0,0]
--- tester.py
+++ tester.py
@@ -50,77 +50,79 @@ def ping_server():
     capacity = args.capacity
     version = args.version
     while True: 
-        if not database or event.is_set():
-            break
-        server = database.pop(0)
-        id = server[0]
-        addr = server[1]
-        port = server[2]
-        server = JavaServer.lookup(f"{addr}:{port}")
-        try:
-            status = server.status()
+        try :
+            if not database or event.is_set():
+                break
+            server = database.pop(0)
+            id = server[0]
+            addr = server[1]
+            port = server[2]
+            server = JavaServer.lookup(f"{addr}:{port}")
+            try:
+                status = server.status()
+            except:
+                continue
+            
+            if not status:
+                continue
+
+            if minplayers and minplayers>=status.players.online:
+                continue
+            if players and players!=status.players.online:
+                continue
+            if maxplayers and maxplayers<=status.players.online:
+                continue
+            if timeout:
+                if timeout <= status.latency:
+                    continue
+            else:
+                if 100 <= status.latency:
+                    continue
+            if capacity and capacity != status.players.max:
+                continue
+            if version and not version in str(status.version.name):
+                continue
+                    
+            players = f"{status.players.online}/{status.players.max}"
+            
+            ip = f"{addr}:{port}"
+            
+            version=str(status.version.name)
+            if len(version) > 27:
+                version = f"{version[:27-3]}..."
+            
+            latency = str(int(status.latency))+"ms"
+
+            description = status.description
+            description = description.replace("§1",colorama.Fore.BLUE)
+            description = description.replace("§2",colorama.Fore.GREEN)
+            description = description.replace("§3",colorama.Fore.CYAN)
+            description = description.replace("§4",colorama.Fore.RED)
+            description = description.replace("§5",colorama.Fore.MAGENTA)
+            description = description.replace("§6",colorama.Fore.YELLOW)
+            description = description.replace("§7",colorama.Fore.LIGHTBLACK_EX)
+            description = description.replace("§8",colorama.Fore.LIGHTBLACK_EX)
+            description = description.replace("§9",colorama.Fore.LIGHTBLUE_EX)
+            description = description.replace("§a",colorama.Fore.LIGHTGREEN_EX)
+            description = description.replace("§b",colorama.Fore.LIGHTCYAN_EX)
+            description = description.replace("§c",colorama.Fore.LIGHTRED_EX)
+            description = description.replace("§d",colorama.Fore.LIGHTMAGENTA_EX)
+            description = description.replace("§e",colorama.Fore.LIGHTYELLOW_EX)
+            description = description.replace("§f",colorama.Fore.WHITE)
+            description = description.replace("\n"," - ")
+            description = description.replace("§k","")
+            description = description.replace("§l",colorama.Style.BRIGHT)
+            description = description.replace("§m","")
+            description = description.replace("§n","\e[4m")
+            description = description.replace("§o","")
+            description = description.replace("§r",colorama.Style.RESET_ALL)
+            
+            if event.is_set():
+                break
+
+            results.append([f"{colorama.Fore.LIGHTRED_EX}{players:10}{colorama.Fore.LIGHTBLUE_EX}{ip:22}{colorama.Fore.LIGHTYELLOW_EX}{version:30}{colorama.Fore.LIGHTGREEN_EX}{latency:6}{colorama.Style.RESET_ALL}{description}{colorama.Style.RESET_ALL}", id])
         except:
-            continue
-        
-        if not status:
-            continue
-
-        if minplayers and minplayers>=status.players.online:
-            continue
-        if players and players!=status.players.online:
-            continue
-        if maxplayers and maxplayers<=status.players.online:
-            continue
-        if timeout:
-            if timeout <= status.latency:
-                continue
-        else:
-            if 100 <= status.latency:
-                continue
-        if capacity and capacity != status.players.max:
-            continue
-        if version and not version in str(status.version.name):
-            continue
-                
-        players = f"{status.players.online}/{status.players.max}"
-        
-        ip = f"{addr}:{port}"
-        
-        version=str(status.version.name)
-        if len(version) > 27:
-            version = f"{version[:27-3]}..."
-        
-        latency = str(int(status.latency))+"ms"
-
-        description = status.description
-        description = description.replace("§1",colorama.Fore.BLUE)
-        description = description.replace("§2",colorama.Fore.GREEN)
-        description = description.replace("§3",colorama.Fore.CYAN)
-        description = description.replace("§4",colorama.Fore.RED)
-        description = description.replace("§5",colorama.Fore.MAGENTA)
-        description = description.replace("§6",colorama.Fore.YELLOW)
-        description = description.replace("§7",colorama.Fore.LIGHTBLACK_EX)
-        description = description.replace("§8",colorama.Fore.LIGHTBLACK_EX)
-        description = description.replace("§9",colorama.Fore.LIGHTBLUE_EX)
-        description = description.replace("§a",colorama.Fore.LIGHTGREEN_EX)
-        description = description.replace("§b",colorama.Fore.LIGHTCYAN_EX)
-        description = description.replace("§c",colorama.Fore.LIGHTRED_EX)
-        description = description.replace("§d",colorama.Fore.LIGHTMAGENTA_EX)
-        description = description.replace("§e",colorama.Fore.LIGHTYELLOW_EX)
-        description = description.replace("§f",colorama.Fore.WHITE)
-        description = description.replace("\n"," - ")
-        description = description.replace("§k","")
-        description = description.replace("§l",colorama.Style.BRIGHT)
-        description = description.replace("§m","")
-        description = description.replace("§n","\e[4m")
-        description = description.replace("§o","")
-        description = description.replace("§r",colorama.Style.RESET_ALL)
-        
-        if event.is_set():
-            break
-
-        results.append([f"{colorama.Fore.LIGHTRED_EX}{players:10}{colorama.Fore.LIGHTBLUE_EX}{ip:22}{colorama.Fore.LIGHTYELLOW_EX}{version:30}{colorama.Fore.LIGHTGREEN_EX}{latency:6}{colorama.Style.RESET_ALL}{description}{colorama.Style.RESET_ALL}", id])
-
+            pass
 
 def Main():
     try:
